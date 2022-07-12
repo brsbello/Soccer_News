2edc5a6ea9fc08239b72f8f2de0862bce88461d6
brsbello <Br!753159>
2022-07-11 21:53:11 -0300

    Ajustes finais.

--- app/src/main/java/com/example/soccernewsbruno/ui/data/SoccerNewsDb.java
+++ app/src/main/java/com/example/soccernewsbruno/ui/data/SoccerNewsDb.java
@@ -4,7 +4,7 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 import com.example.soccernewsbruno.domain.News;
 
-@Database(entities = {News.class}, version = 1)
+@Database(entities = {News.class}, version = 1, exportSchema = false)
 public abstract class SoccerNewsDb extends RoomDatabase {
     public abstract NewsDao newsDao();
 }
